59945931f2f7fb755b14e503736372a00e162aa7
Siavash Askari Nasr <siavash.askari.nasr@gmail.com>
2013-12-19 16:39:16 +0330

    Remove jQuery dependency.

--- data/js/panel.js
+++ data/js/panel.js
@@ -1,6 +1,20 @@
-$('form').submit(function() {
-    $('#result').hide();
-    let magnet = $('#magnet').val();
+let result = document.getElementById('result');
+let form = document.forms['magrent-form'];
+let magnetInput = document.getElementById('magnet');
+let submitBtn = document.getElementById('submit');
+
+let showResult = output => {
+    result.innerHTML = output;
+    // result.style.opacity = 1;
+    // result.classList.remove('fade-out');
+    // result.classList.add('fade-in');
+};
+
+form.addEventListener('submit', function(e) {
+    // result.style.opacity = 0;
+    // result.classList.remove('fade-in');
+    // result.classList.add('fade-out');
+    let magnet = magnetInput.value;
     let torrent = magrent(magnet, true);
     if (torrent) {
         // List of torrent cache sites
@@ -20,43 +34,42 @@ $('form').submit(function() {
                 escapeHTML(torrentSites[i]) +
                 '</a>\n';
         }
-        showResult('<p>' +
-                   (!torrent.name ? '.torrent file' : '<span class="torrentName"></span>') +
-                   '</p>\n<p>' +
+        showResult('<div class="fade-in"><p>' +
+                   (!torrent.name ? '.torrent file' : '<span id="torrentName"></span>') +
+                   '</p><p>' +
                    torrentLinks +
-                   '</p>\n');
+                   '</p></div>');
         // If torrent has a name
-        if(torrent.name) $('.torrentName').text(decodeURIComponent(torrent.name));
+        if(torrent.name)
+            document.getElementById('torrentName').textContent = decodeURIComponent(torrent.name);
     }
     else {
-        showResult('<p>Enter a valid magnet URI</p>');
+        showResult('<p class="fade-in">Enter a valid magnet URI</p>');
     }
 
-    return false;
-});
-
-$('#result').on('click', '.torrentLink', function (e) {
-    let $this = $(this);
-    self.port.emit('downloadTorrent', {
-        name: $this.data('name') ? decodeURIComponent($this.data('name')) : '',
-        hash: $this.data('hash'),
-        url: $this.attr('href')
-    });
     e.preventDefault();
 });
 
-function showResult(result) {
-    $('#result').html(result);
-    $('#result').stop(true, true).fadeIn('slow');
-}
+result.addEventListener('click', function (e) {
+    if (e.target.classList.contains('torrentLink')) {
+        let data = e.target.dataset;
+        self.port.emit('downloadTorrent', {
+            name: data.name ? decodeURIComponent(data.name) : '',
+            hash: data.hash,
+            url: e.target.href
+        });
+    }
+
+    e.preventDefault();
+});
 
 self.port.on('convert', function(magnet) {
-    if($('#magnet').val() !== magnet) {
-        $('#magnet').val(magnet);
-        $('form').submit();
+    if(magnetInput.value !== magnet) {
+        magnetInput.value = magnet;
+        submitBtn.click();
     }
 });
 
 self.port.on('focus', function() {
-    $('#magnet').focus();
+    magnetInput.focus();
 });
