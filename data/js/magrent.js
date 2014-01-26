641390a7942c1cad13532d4fccfc6ac4dbb91dfc
Siavash Askari Nasr <siavash.askari.nasr@gmail.com>
2014-01-26 23:26:57 +0330

    Add license and license notices.

--- data/js/magrent.js
+++ data/js/magrent.js
@@ -1,3 +1,22 @@
+/*
+ * Copyright (C) 2014  Siavash Askari Nasr
+ *
+ * This file is part of Magrent.
+ *
+ * Magrent is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Magrent is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Magrent.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 // Constructing base32 and base16 encoder/decoder
 const base32 = new Nibbler({
     dataBits: 8,
